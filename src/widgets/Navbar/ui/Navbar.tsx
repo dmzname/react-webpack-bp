f230a4078ca328da9c8603c1f9f41f737353d7c1
Dmitry Zabelin <dmz.name@gmail.com>
2023-07-04 22:02:32 +0300

    added stylelint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -13,7 +13,7 @@ export const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={ classNames(cls.root, {}, [ className ]) }>
             <div className={ cls.links }>
-                <AppLink theme={ AppLinkTheme.SECONDARY } to="/" className={ cls.mainLink }>
+                <AppLink theme={ AppLinkTheme.SECONDARY } to="/" className={ cls['main-link'] }>
                     {t('Главная')}
                 </AppLink>
                 <AppLink theme={ AppLinkTheme.RED } to="/about">
