7dbe64c2957b76f791e57cdd9e52f9da4c9b028d
dmzname <dmz.name@gmail.com>
2023-07-16 13:21:28 +0300

    fixed unit testing

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.spec.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.spec.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from "shared/lib/tests/componentRender/componentRender";
 
 describe('Sidebar', () => {
     it('Test render', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     it('Test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
