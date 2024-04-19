33f8ee7b11a04590e38f189070a6facf95ae0537
Dm.Zabelin <dmz.name@gmail.com>
2024-04-19 21:10:29 +0300

    fixed ui

--- src/features/ArticleCommentList/ui/CommentList/CommentList.stories.tsx
+++ src/features/ArticleCommentList/ui/CommentList/CommentList.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { CommentList } from "./CommentList";
+import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 const meta: Meta<typeof CommentList> = {
     title: 'entities/CommentList',
@@ -10,4 +11,8 @@ export default meta;
 
 type Story = StoryObj<typeof CommentList>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+    decorators: [
+        StoreDecorator({})
+    ]
+};
